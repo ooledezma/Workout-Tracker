ae647c84285d0bc33ad326e5dc5d80841310e482
Oscar Ledezma <oscar.ledezma@nusano.com>
2020-11-01 21:47:45 -0800

    updated routes

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const db = require("../models");
 
+
 //GET route for "/api/workouts"
 router.get("/api/workouts", (req, res) => {
   db.Workout.find({})
@@ -13,18 +14,16 @@ router.get("/api/workouts", (req, res) => {
   });
 
 //PUT route for "/api/workouts/:id"
-router.put("/api/workouts/:id", ({body, params}, res) => {
-  db.Workout.findOneAndUpdate({_id: params.id}, {$push: {exercises: body}}, {new: true})
-  .then(dbWorkout => {
-    console.log(dbWorkout)
-      res.json(dbWorkout);
-
-  })
-  .catch(err => {
-      res.json(err);
-  });
-  });
-
+router.put("/api/workouts/:id", function ({ body, params }, res){
+  db.Workout.findByIdAndUpdate(
+      params.id,
+      {$push: {exercises: body}},
+      { new: true, runValidators: true}).then(dbWorkout => {
+          res.json(dbWorkout);
+      }).catch(err => {
+        res.status(400).json(err);
+      });
+    });
 
 
 //POST route for "/api/workouts"
@@ -50,6 +49,7 @@ router.get("/api/workouts/range", (req, res) => {
   });
   });
 
+//POST route for "/api/workouts/range"
  router.post("/api/workouts/range", (req, res) => {
     db.Workout.find({})
       .then(dbWorkout => {
